ee261385871ed9bf827da3a4855e29f4ea147dec
caiocotrim <caiocotrimp@gmail.com>
2025-12-29 11:22:03 -0300

    add dados de quimica

--- versoes/v1.1/app.py
+++ versoes/v1.1/app.py
@@ -18,14 +18,14 @@ docs_txt_civil = loader_civil.load()
 
 loader_ambiental = DirectoryLoader("../../base-de-dados/dados-tratados/ambiental", glob="*.txt", loader_cls=TextLoader, loader_kwargs={"encoding": "utf-8"})
 docs_txt_ambiental = loader_ambiental.load()
-# Loader dos cursos abaixos estão comentados pois ainda não há dados tratados para eles
-"""
+
 loader_eletrica = DirectoryLoader("../../base-de-dados/dados-tratados/eletrica", glob="*.txt", loader_cls=TextLoader, loader_kwargs={"encoding": "utf-8"})
 docs_txt_eletrica = loader_eletrica.load()
 
 loader_quimica = DirectoryLoader("../../base-de-dados/dados-tratados/quimica", glob="*.txt", loader_cls=TextLoader, loader_kwargs={"encoding": "utf-8"})
 docs_txt_quimica = loader_quimica.load()
-
+# Loader dos cursos abaixos estão comentados pois ainda não há dados tratados para eles
+"""
 loader_geral = DirectoryLoader("../../base-de-dados/dados-tratados/geral", glob="*.txt", loader_cls=TextLoader, loader_kwargs={"encoding": "utf-8"})
 docs_txt_geral = loader_geral.load()
 """
@@ -35,20 +35,20 @@ embeddings = OpenAIEmbeddings()
 faiss_bsi = FAISS.from_documents(docs_txt_bsi, embeddings)
 faiss_civil = FAISS.from_documents(docs_txt_civil, embeddings)
 faiss_ambiental = FAISS.from_documents(docs_txt_ambiental, embeddings)
-# Base de dados dos cursos abaixos estão comentados pois ainda não há dados tratados para eles
-"""
 faiss_eletrica = FAISS.from_documents(docs_txt_eletrica, embeddings)
 faiss_quimica = FAISS.from_documents(docs_txt_quimica, embeddings)
+# Base de dados dos cursos abaixos estão comentados pois ainda não há dados tratados para eles
+"""
 faiss_geral = FAISS.from_documents(docs_txt_geral, embeddings)
 """
 
 retriever_bsi = faiss_bsi.as_retriever()
 retriever_civil = faiss_civil.as_retriever()
 retriever_ambiental = faiss_ambiental.as_retriever()
-# Retrievers dos cursos abaixos estão comentados pois ainda não há dados tratados para eles
-"""
 retriever_eletrica = faiss_eletrica.as_retriever()
 retriever_quimica = faiss_quimica.as_retriever()
+# Retrievers dos cursos abaixos estão comentados pois ainda não há dados tratados para eles
+"""
 retriever_geral = faiss_geral.as_retriever()
 """
 
@@ -71,13 +71,13 @@ def escolher_retriever(mensagem):
     if "civil" in msg or "engenharia civil" in msg or "eng civil" in msg or "eng. civil" in msg:
         return retriever_civil    
     if "ambiental" in msg or "engenharia ambiental" in msg or "eng ambiental" in msg or "eng. ambiental" in msg:
-        return retriever_ambiental
+        return retriever_ambiental    
+    if "elétrica" in msg or "engenharia elétrica" in msg or "eng elétrica" in msg or "eng. elétrica" in msg or "eletrica" in msg or "elétrica" in msg:
+        return retriever_eletrica
+    if "química" in msg or "licenciatura em química" in msg or "lic química" in msg or "lic. química" in msg or "quimica" in msg or "química" in msg:
+        return retriever_quimica
     # Retrievers dos cursos abaixos estão comentados pois ainda não há dados tratados para eles
     """
-    if "elétrica" in msg or "engenharia elétrica" in msg or "eng elétrica" in msg or "eng. elétrica" in msg:
-        return retriever_eletrica
-    if "química" in msg or "engenharia química" in msg or "eng química" in msg or "eng. química" in msg:
-        return retriever_quimica
     else:
         return retriever_geral
     """    
